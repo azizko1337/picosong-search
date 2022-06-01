01b7084277d5eaa57ed453e04cc5b3b2886f8718
azizko1337 <antoni.zalupka123@gmail.com>
2022-06-01 22:16:23 +0200

    Added roboto downloading

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import GlobalStyle from "../styles/GlobalStyle";
 import Wrapper from "../components/Wrapper";
 import MaxWidth from "../components/MaxWidth";
@@ -8,6 +9,14 @@ function App(props) {
   const { Component, pageProps } = props;
   return (
     <Wrapper>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <GlobalStyle />
       <MaxWidth>
         <Component {...pageProps} />
